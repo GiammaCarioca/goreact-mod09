cff39427fdaec31ce259ef1e0081ee24bc2e2783
Thiago Giammattey <giammattey@gmail.com>
2019-07-19 17:17:45 -0300

    estilizando notificações

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import logo from '~/assets/logo-purple.svg';
+import Notifications from '~/components/Notifications';
 
+import logo from '~/assets/logo-purple.svg';
 import { Container, Content, Profile } from './styles';
 
 export default function Header() {
@@ -13,14 +14,17 @@ export default function Header() {
           <img src={logo} alt="GoBarber" />
           <Link to="/dashboard">DASHBOARD</Link>
         </nav>
+
         <aside>
+          <Notifications />
+
           <Profile>
             <div>
               <strong>Diego Fernandes</strong>
               <Link to="/profile">Meu perfil</Link>
             </div>
             <img
-              src="https://api.adorable.io/avatars/50/abott@adorable.pngCopy to Clipboard"
+              src="https://api.adorable.io/avatars/50/abott@adorable.png"
               alt="Diego Fernandes"
             />
           </Profile>
